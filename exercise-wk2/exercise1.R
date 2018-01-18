fc71abdad2061b05285e0f29ad13326d78cf723d
Rani-Chang <Yuntsye@gmail.com>
2018-01-17 17:07:16 -0800

    Participation points

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -1,15 +1,17 @@
 ## Part 1: Debugging
 
 my.num <- 6
-initials <- "?. ?."
+initials <- "YTC"
 
-my.vector <- c(my.num, initials)
+my.vector <- c(6, "YTC")
+print(my.vector)
 
 # run ?sum to get more info
 vector.sum <- sum(my.vector)
+print(vector.sum)
 
 # Describe why this doesn't work: 
-# 
+# object 'vector.sum' not found, doubles (decimal) and characters(strings) only 
 
 install.packages("stringr")
 
@@ -18,24 +20,33 @@ my.line <- "Hey, hey, this is the library"
 print(str_length(my.line))
 
 # Describe why this doesn't work: 
-# 
+# Didn't load the library at first 
 
-said.the.famous <- paste(my.line, " - ", initial)
+said.the.famous <- paste(my.line, " - ", initials)
 
 # Describe why this doesn't work: 
-# 
+# object 'initial' not found, is not initial but initials (basically a typo)
 
 
 ## Part 2 - Vector and function practice
 
-# Make a vector and use typeof to check what type R considers it to be
+# Make a vector and use type of to check what type R considers it to be
+typeof(c("dogs", "cats", "ferrets")) #
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
 
+CompareLength <- function(v1, v2) {
+  diff <- (length(v1)-length(v2))
+  phrase <- paste("The difference in lengths is", diff)
+  return(phrase)
+}
+v1 <- c(1,2,3,4,5)
+v2 <- c(1,2,3)
+print(CompareLength(v2,v1))
 
 # Pass two vectors of different length to your `CompareLength` function
-
+  
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
